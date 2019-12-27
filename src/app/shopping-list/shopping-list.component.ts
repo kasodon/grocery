b8bb1e2ea52e47e0eee32aab3c462f20f6f6f285
Edewor Onyedika <edwordkaso100@gmail.com>
2019-12-27 03:30:53 +0100

    ingredients implemented

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.module';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  Ingredients: Ingredient[] = [
+    new Ingredient('siriora cut food', 200),
+    new Ingredient('Onion', 10)
+  ];
 
   constructor() { }
 
