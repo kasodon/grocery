ded018ed1ea2e54d9187259ac39676a9ef631e62
Edewor Onyedika <edwordkaso100@gmail.com>
2019-12-27 02:16:54 +0100

    recipe list

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../recipe.module';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is just a test description', 'https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-3.jpg')
+    new Recipe('Test Recipe', 'This is just a test description', 'https://whereismyspoon.co/wp-content/uploads/2018/07/jollof-rice-3.jpg'),
+    new Recipe('Jollof Rice', 'The Best Party Jollof Rice Recipe You Can’t Resist', 'https://elleyajoku.com/wp-content/uploads/2017/10/jollof-rice-recipe_.jpg')
   ];
 
   constructor() { }
